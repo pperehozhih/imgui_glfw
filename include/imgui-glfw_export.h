89446b0ae4b9b96614bddac24b61802152dd5efe
Pavel Perekhozhikh <p.perehogih@gmail.com>
2021-01-08 11:22:51 +0300

    Dev commit

--- include/imgui-glfw_export.h
+++ include/imgui-glfw_export.h
@@ -1,24 +1,24 @@
-#ifndef IMGUI_SFML_EXPORT_H
-#define IMGUI_SFML_EXPORT_H
+#ifndef IMGUI_GLFW_EXPORT_H
+#define IMGUI_GLFW_EXPORT_H
 
-#if IMGUI_SFML_SHARED_LIB
+#if IMGUI_GLFW_SHARED_LIB
     #if _WIN32
-        #ifdef IMGUI_SFML_EXPORTS
-            #define IMGUI_SFML_API __declspec(dllexport)
+        #ifdef IMGUI_GLFW_EXPORTS
+            #define IMGUI_GLFW_API __declspec(dllexport)
             #define IMGUI_API __declspec(dllexport)
         #else
-            #define IMGUI_SFML_API __declspec(dllimport)
+            #define IMGUI_GLFW_API __declspec(dllimport)
             #define IMGUI_API __declspec(dllexport)
         #endif
     #elif __GNUC__ >= 4
-        #define IMGUI_SFML_API __attribute__ ((visibility("default")))
+        #define IMGUI_GLFW_API __attribute__ ((visibility("default")))
         #define IMGUI_API __attribute__ ((visibility("default")))
     #else
-        #define IMGUI_SFML_API
+        #define IMGUI_GLFW_API
         #define IMGUI_API
     #endif
 #else
-    #define IMGUI_SFML_API
+    #define IMGUI_GLFW_API
     #define IMGUI_API
 #endif
 
